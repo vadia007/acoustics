d76c8d52921b48a540c063359d90acce5c80ef37
vadia <vaaadik@mail.ru>
2015-07-24 22:19:00 +0300

    before products

--- wp-content/themes/twentyfifteen/functions.php
+++ wp-content/themes/twentyfifteen/functions.php
@@ -359,3 +359,12 @@ require get_template_directory() . '/inc/template-tags.php';
  * @since Twenty Fifteen 1.0
  */
 require get_template_directory() . '/inc/customizer.php';
+
+//Gets post cat slug and looks for single-[cat slug].php and applies it
+add_filter('single_template', create_function(
+        '$the_template',
+        'foreach( (array) get_the_category() as $cat ) {
+		if ( file_exists(TEMPLATEPATH . "/single-{$cat->slug}.php") )
+		return TEMPLATEPATH . "/single-{$cat->slug}.php"; }
+	return $the_template;' )
+);
--- wp-content/themes/twentyfifteen/single-news.php
+++ wp-content/themes/twentyfifteen/single-news.php
@@ -3,6 +3,7 @@
 <div id="content" class="news-page">
     <div class="inside">
         <div class="interior-top">
+            <?php $a = get_the_category($post->ID)?>
             <h1><?php the_title(); ?></h1>
         </div>
     </div>
